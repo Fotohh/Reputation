17abd4a02113870c9394fe8e988a400b3b85e21d
XaxisPlayz <66885524+XaxisPlayz@users.noreply.github.com>
2022-12-27 01:12:53 -0800

    barebones

---
+++ src/main/java/me/xaxis/reputation/Reputation.java
@@ -0,0 +1,17 @@
+package me.xaxis.reputation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Reputation extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

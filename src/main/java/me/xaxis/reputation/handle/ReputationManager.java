b29a04f4c54cfbd202e08a8a78bb8984dfa7c30e
XaxisPlayz <66885524+XaxisPlayz@users.noreply.github.com>
2022-12-31 11:23:30 -0800

    changes

--- src/main/java/me/xaxis/reputation/handle/ReputationManager.java
+++ src/main/java/me/xaxis/reputation/handle/ReputationManager.java
@@ -1,2 +1,13 @@
-package me.xaxis.reputation.handle;public class ReputationManager {
+package me.xaxis.reputation.handle;
+
+import me.xaxis.reputation.Reputation;
+
+public class ReputationManager {
+
+    private final Reputation plugin;
+
+
+    public ReputationManager(Reputation plugin) {
+        this.plugin = plugin;
+    }
 }
